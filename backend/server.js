06e4210259e5bfe004a2109563fc675798b4f07e
Gaurav <ec23020@glbitm.ac.in>
2026-09-25 13:48:46 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -878,9 +878,8 @@ app.post("/api/auth/google", async (req, res) => {
 // =========================
 // START SERVER
 // =========================
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log(
-    "Server running on http://localhost:5000"
-  );
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
